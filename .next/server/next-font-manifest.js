25e4e946718a030ef5ddb07ee4931e7285a007fd
kunal <kunalkhelkar123@gmail.com>
2025-01-30 12:28:48 +0530

    30/01/2025

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":[],"/DigitalBusinessCardFeatures":[],"/Footer":[],"/contact":[],"/create-profile":[],"/":[]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
